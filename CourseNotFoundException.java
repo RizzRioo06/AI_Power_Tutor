5d7e686126785f9150e0294a6de68b4f41728672
RizzRioo06 <nikk3.0falone@gmail.com>
2025-04-17 14:12:33 +0700

    Delete CourseNotFoundException.java

--- CourseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.aitutor.exception;
-
-public class CourseNotFoundException extends Exception {
-    public CourseNotFoundException(String courseId) {
-        super("Course not found with ID: " + courseId);
-    }
-}
